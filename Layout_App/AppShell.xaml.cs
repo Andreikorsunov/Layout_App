cd64f60bbe4176bd00d292f3e064e4e9e61570b6
Andreikorsunov <77333208+Andreikorsunov@users.noreply.github.com>
2022-02-25 11:06:15 +0200

    nnn

---
+++ Layout_App/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Layout_App.ViewModels;
+using Layout_App.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Layout_App
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ Layout_App/Clicker.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Layout_App
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Clicker : ContentPage
+    {
+        Label lb;
+        BoxView box;
+
+        public Clicker()
+        {
+            this.BackgroundColor = Color.White;
+            lb = new Label()
+            {
+                BackgroundColor = Color.Black,
+                TextColor = Color.White,
+                HorizontalOptions = LayoutOptions.Start
+
+            };
+
+            box = new BoxView()
+            {
+                Color = Color.Red,
+                CornerRadius = 1000,
+                WidthRequest = 300,
+                HeightRequest = 300,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+            TapGestureRecognizer tap = new TapGestureRecognizer();
+            tap.Tapped += Tap_Tapped;
+            box.GestureRecognizers.Add(tap);
+
+            StackLayout st = new StackLayout { Children = { box } };
+            Content = st;
+            st.Children.Add(lb);
+        }
+        int i = 0;
+        private void Tap_Tapped(object sender, EventArgs e)
+        {
+            i++;
+            lb.Text = "Sa vajutatud " + i + " korda";
+            if (i >= 1)
+            {
+                try
+                {
+                    // Use default vibration length
+                    Vibration.Vibrate();
+
+                    // Or use specified time
+                    var duration = TimeSpan.FromSeconds(0.2);
+                    Vibration.Vibrate(duration);
+                }
+                catch (FeatureNotSupportedException ex)
+                {
+                    // Feature not supported on device
+                }
+                catch (Exception ex)
+                {
+                    // Other error has occurred.
+                }
+            }
+        }
+    }
+}
